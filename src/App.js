9920c85fb99187b9bc711673c5b138312b791157
Henrique Omena <88lmao@gmail.com>
2019-08-25 20:54:00 +0100

    Fetch some names in browser

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        // apenas para relembrar ele busca o construtor de Component que foi extendido em App
+        super();
+
+        this.state = {
+            monsters: [
+                {
+                    name: 'Frankenstein',
+                    id: 'm1'
+                },
+                {
+                    name: 'Dracul',
+                    id: 'm2'
+                },
+                {
+                    name: 'Zombie',
+                    id: 'm3'
+                },
+            ]
+        }
+
+    }
+
+    render() {
+        return (
+            <div className="App">
+                {
+                    this.state.monsters.map( monster => {
+                        return <h1 key={monster.id} > { monster.name } </h1>
+                    })
+                }
+            </div>
+        );
+    }
 }
 
 export default App;
